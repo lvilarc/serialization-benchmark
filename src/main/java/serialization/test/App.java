10bc6bc4d49bf01b1d756c4f9c167dfe65dbba74
Lucas Vilar <lucas.vilar.carneiro@gmail.com>
2026-02-24 16:31:34 +0000

    New Project with Maven

---
+++ src/main/java/serialization/test/App.java
@@ -0,0 +1,13 @@
+package serialization.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
